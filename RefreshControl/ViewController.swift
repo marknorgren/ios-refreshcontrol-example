a3020f1925a6694bd246a670fbf0f762074895b8
Mark Norgren <mark@marknorgren.com>
2021-03-10 10:54:47 -0600

    Quick RefreshControl test

--- RefreshControl/ViewController.swift
+++ RefreshControl/ViewController.swift
@@ -9,9 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    let refreshControl = UIRefreshControl()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        tableView.refreshControl = refreshControl
+    }
+
+    @objc func refresh() {
+        print("refresh")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { [weak self] in
+            self?.refreshControl.endRefreshing()
+        }
+    }
+}
+
+
+extension ViewController: UITableViewDelegate {
+
+}
+
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
     }
 
 
